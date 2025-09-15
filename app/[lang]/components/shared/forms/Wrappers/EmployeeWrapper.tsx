a16b8130607634f04d22fee0bfc835c0ffd6d007
AkwasiAddai14 <akwasivdsm@gmail.com>
2025-09-15 23:28:28 +0200

    juntereu

--- app/[lang]/components/shared/forms/Wrappers/EmployeeWrapper.tsx
+++ app/[lang]/components/shared/forms/Wrappers/EmployeeWrapper.tsx
@@ -1,12 +1,15 @@
-import { auth, currentUser } from "@clerk/nextjs/server";
+'use client';
+
+import { useUser } from '@clerk/nextjs';
 import { getDictionary } from "@/app/[lang]/dictionaries";
 import EmployeeFormClient from "@/app/[lang]/components/shared/forms/EmployeeForm";
 import type { Locale } from '@/app/[lang]/dictionaries'; // define this type based on keys
 
 
 export default async function EmployeeFormServer({ lang }: { lang: Locale }) {
-  //const { userId } = await auth();
-  const user = await currentUser();
+  const { user, isLoaded } = useUser();
+  if (!isLoaded) return null; // or a small skeleton
+
   const dictionary = await getDictionary(lang);
 
   return (
