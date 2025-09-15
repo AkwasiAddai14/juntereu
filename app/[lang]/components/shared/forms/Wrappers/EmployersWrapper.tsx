e69a7996847cd232dfc30a1b0490da687689c277
AkwasiAddai14 <akwasivdsm@gmail.com>
2025-09-15 23:33:55 +0200

    juntereu

--- app/[lang]/components/shared/forms/Wrappers/EmployersWrapper.tsx
+++ app/[lang]/components/shared/forms/Wrappers/EmployersWrapper.tsx
@@ -1,15 +1,17 @@
+'use client';
+
+import { useUser } from '@clerk/nextjs';
 import { getDictionary } from '@/app/[lang]/dictionaries';
 import type { Locale } from '@/app/[lang]/dictionaries'; // define this type based on keys
 import BedrijfsFormClient from '@/app/[lang]/components/shared/forms/CompanyForm';
-import { auth, currentUser } from '@clerk/nextjs/server';
 
 interface Props {
   lang: Locale;
 }
 
 const BedrijfsFormWrapper = async ({ lang }: Props) => {
-  //const { userId } = await auth();
-  const user = await currentUser();
+  const { user, isLoaded } = useUser();
+  if (!isLoaded) return null; // or a small skeleton
   const { components, Validations } = await getDictionary(lang);
 
   return (
