90817f3dabc9ca68e3d848e66f886be08e695673
AkwasiAddai14 <akwasivdsm@gmail.com>
2025-09-19 20:00:35 +0200

    juntereu

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+export default clerkMiddleware()
 
 import { Locale, i18n } from '@/i18n.config'
 
@@ -65,6 +68,13 @@ if (!isValidLocale) {
   return NextResponse.next();
 };
 
+
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
