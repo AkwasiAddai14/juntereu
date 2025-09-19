1abe8676e7a80beee32bc631b1b9395b92b05f5f
AkwasiAddai14 <akwasivdsm@gmail.com>
2025-09-19 21:12:11 +0200

    juntereu

--- app/[lang]/dashboard/availability/create/page.tsx
+++ app/[lang]/dashboard/availability/create/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+/* "use client"
 
 import { useUser } from "@clerk/nextjs";
 import DashNav from "@/app/[lang]/components/shared/navigation/Wrappers/NavigationWrapper";
@@ -36,4 +36,4 @@ const MaakVacature = ({ params }: { params: { lang: string } }) => {
   )
 }
 
-export default MaakVacature;
+export default MaakVacature; */
